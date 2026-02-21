8038bcf22dd3eabb1fb4b2aa6f935791c6397079
Chhun-Nika <chhunnika488@gmail.com>
2026-02-22 01:35:42 +0700

    TASK-002 - Downloader - Color Theme

--- lib/2_download_app/ui/providers/theme_color_provider.dart
+++ lib/2_download_app/ui/providers/theme_color_provider.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 enum ThemeColor {
   blue(color: Color.fromARGB(255, 34, 118, 229)),
-  green(color: Color.fromARGB(255, 229, 158, 221)),
-  pink(color: Color.fromARGB(255, 156, 202, 8));
+  pink(color: Color.fromARGB(255, 229, 158, 221)),
+  green(color: Color.fromARGB(255, 156, 202, 8));
 
   const ThemeColor({required this.color});
 
@@ -11,4 +11,17 @@ enum ThemeColor {
   Color get backgroundColor => color.withAlpha(100);
 }
 
-ThemeColor currentThemeColor = ThemeColor.blue;
+// ThemeColor currentThemeColor = ThemeColor.pink;
+
+class ThemeColorNotifier extends ChangeNotifier {
+  ThemeColor _theme = ThemeColor.blue;
+
+  ThemeColor get currentTheme => _theme;
+
+  void onChange(ThemeColor newColor) {
+    _theme = newColor;
+    notifyListeners();
+  }
+}
+
+ThemeColorNotifier currentThemeColor = ThemeColorNotifier();
